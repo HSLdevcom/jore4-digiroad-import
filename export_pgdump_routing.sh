HSLdevcom/jore4-digiroad-import-experiment

Create separate schema with road link topology using pgRouting extension.

export_pgdump_routing.sh
@@ -1,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Stop on first error. Have meaningful error messages. Print each command to stdout.
+set -euxo pipefail
+
+# Source common environment variables.
+source "$(cd "$(dirname "$0")"; pwd -P)/set_env_vars.sh"
+
+# Start Docker container. The container is expected to exist and contain all the data to be exported.
+docker start $DOCKER_CONTAINER
+
+# Wait for PostgreSQL to start.
+docker run -it --rm --link "${DOCKER_CONTAINER}":postgres $DOCKER_IMAGE sh -c "$PG_WAIT"
+
+PGDUMP_OUTPUT="digiroad_k_routing_$(date "+%Y-%m-%d").pgdump"
+OUTPUT_TABLES="dr_link_id dr_linkki_k dr_linkki_k_vertices_pgr dr_pysakki"
+OUTPUT_TABLE_OPTIONS="`echo ${OUTPUT_TABLES[@]} | sed \"s/dr_/-t ${DB_ROUTING_SCHEMA_NAME}.dr_/g\"`"
+
+# Export pg_dump file.
+docker run -it --rm --link "${DOCKER_CONTAINER}":postgres -v ${WORK_DIR}/pgdump/:/tmp/pgdump $DOCKER_IMAGE \
+  sh -c "$PG_DUMP -Fc --clean -f /tmp/pgdump/${PGDUMP_OUTPUT} $OUTPUT_TABLE_OPTIONS"
+
+# Stop Docker container.
+docker stop $DOCKER_CONTAINER

import_digiroad_shapefiles.sh
@@ -76,7 +76,7 @@ docker run -it --rm --link "${DOCKER_CONTAINER}":postgres -v ${CWD}/sql:/tmp/sql
 
 # Process road geometries and filtering properties in database.
 docker run -it --rm --link "${DOCKER_CONTAINER}":postgres -v ${CWD}/sql:/tmp/sql \
-  ${DOCKER_IMAGE} sh -c "$PSQL -v ON_ERROR_STOP=1 -f /tmp/sql/transform_dr_linkki_k.sql -v schema=${DB_IMPORT_SCHEMA_NAME}"
+  ${DOCKER_IMAGE} sh -c "$PSQL -v ON_ERROR_STOP=1 -f /tmp/sql/transform_dr_linkki_k.sql -v schema=${DB_IMPORT_SCHEMA_NAME} -v sql_dir=/tmp/sql"
 
 # Load DR_PYSAKKI shapefile into database.
 docker run -it --rm --link "${DOCKER_CONTAINER}":postgres -v ${SHP_FILE_DIR}:/tmp/shp $DOCKER_IMAGE \
@@ -84,11 +84,15 @@ docker run -it --rm --link "${DOCKER_CONTAINER}":postgres -v ${SHP_FILE_DIR}:/tm
 
 # Process stops and filter properties in database.
 docker run -it --rm --link "${DOCKER_CONTAINER}":postgres -v ${CWD}/sql:/tmp/sql \
-  ${DOCKER_IMAGE} sh -c "$PSQL -v ON_ERROR_STOP=1 -f /tmp/sql/transform_dr_pysakki.sql -v schema=${DB_IMPORT_SCHEMA_NAME}"
+  ${DOCKER_IMAGE} sh -c "$PSQL -v ON_ERROR_STOP=1 -f /tmp/sql/transform_dr_pysakki.sql -v schema=${DB_IMPORT_SCHEMA_NAME} -v sql_dir=/tmp/sql"
 
 # Process turn restrictions and filter properties in database.
 docker run -it --rm --link "${DOCKER_CONTAINER}":postgres -v ${CWD}/sql:/tmp/sql \
   ${DOCKER_IMAGE} sh -c "$PSQL -v ON_ERROR_STOP=1 -f /tmp/sql/transform_dr_kaantymisrajoitus_k.sql -v schema=${DB_IMPORT_SCHEMA_NAME}"
+
+# Create routing schema containing pgRouting topology.
+docker run -it --rm --link "${DOCKER_CONTAINER}":postgres -v ${CWD}/sql:/tmp/sql \
+  ${DOCKER_IMAGE} sh -c "$PSQL -v ON_ERROR_STOP=1 -f /tmp/sql/create_routing_schema.sql -v source_schema=${DB_IMPORT_SCHEMA_NAME} -v schema=${DB_ROUTING_SCHEMA_NAME} -v sql_dir=/tmp/sql"
 
 # Create separate schema for exporting data in MBTiles format.
 docker run -it --rm --link "${DOCKER_CONTAINER}":postgres -v ${CWD}/sql:/tmp/sql \

sql/create_routing_schema.sql
@@ -1,0 +1,77 @@
+DROP SCHEMA IF EXISTS :schema CASCADE;
+
+CREATE SCHEMA :schema;
+
+-- Copy dr_link_id table from source schema and add constraints.
+CREATE TABLE :schema.dr_link_id (LIKE :source_schema.dr_link_id INCLUDING INDEXES);
+INSERT INTO :schema.dr_link_id SELECT * FROM :source_schema.dr_link_id;
+
+CREATE TABLE :schema.dr_linkki_k AS
+SELECT
+    src.gid,
+    src.link_id,
+    src.segm_id,
+    src.kuntakoodi,
+    src.linkkityyp,
+    src.ajosuunta,
+    src.link_tila,
+    src.tienimi_su,
+    src.tienimi_ru,
+    src.tienimi_sa,
+    ST_Force3D(src.geom) AS geom_3d
+FROM :source_schema.dr_linkki_k src;
+
+ALTER TABLE :schema.dr_linkki_k ADD COLUMN source INTEGER;
+ALTER TABLE :schema.dr_linkki_k ADD COLUMN target INTEGER;
+
+ALTER TABLE :schema.dr_linkki_k ADD COLUMN cost FLOAT;
+ALTER TABLE :schema.dr_linkki_k ADD COLUMN reverse_cost FLOAT;
+
+-- Add 2D geometry to be used with pgRouting extension.
+SELECT AddGeometryColumn(:'schema', 'dr_linkki_k', 'geom', 3067, 'LINESTRING', 2);
+UPDATE :schema.dr_linkki_k SET geom = ST_Force2D(geom_3d);
+ALTER TABLE :schema.dr_linkki_k ALTER COLUMN geom SET NOT NULL;
+
+-- Add data integrity constraints to dr_linkki_k table.
+\i :sql_dir/add_dr_linkki_k_constraints.sql
+
+-- Copy dr_pysakki table from source schema and add constraints.
+CREATE TABLE :schema.dr_pysakki (LIKE :source_schema.dr_pysakki);
+INSERT INTO :schema.dr_pysakki SELECT * FROM :source_schema.dr_pysakki;
+\i :sql_dir/add_dr_pysakki_constraints.sql
+\i :sql_dir/link_dr_pysakki_to_dr_linkki_k.sql
+
+-- Create pgRouting topology.
+
+CREATE INDEX dr_linkki_k_geom_idx ON :schema.dr_linkki_k USING GIST(geom);
+CREATE INDEX dr_linkki_k_source_idx ON :schema.dr_linkki_k (source);
+CREATE INDEX dr_linkki_k_target_idx ON :schema.dr_linkki_k (target);
+
+-- Need to use `gid` as ID column instead of `link_id` because pgRouting requires integer based ID column.
+SELECT pgr_createTopology(:'schema' || '.dr_linkki_k', 0.001, 'geom', 'gid');
+ALTER TABLE :schema.dr_linkki_k ALTER COLUMN source SET NOT NULL;
+ALTER TABLE :schema.dr_linkki_k ALTER COLUMN target SET NOT NULL;
+
+-- Set up cost and reverse_cost parameters for pgRouting functions.
+-- TODO: Do cost calculation based on speed limits.
+
+UPDATE :schema.dr_linkki_k
+SET cost = (
+    CASE
+        WHEN ajosuunta IN (2,4) THEN ST_Length(geom_3d)
+        ELSE -1
+    END
+);
+
+UPDATE :schema.dr_linkki_k
+SET reverse_cost = (
+    CASE
+        WHEN ajosuunta IN (2,3) THEN ST_Length(geom_3d)
+        ELSE -1
+    END
+);
+
+ALTER TABLE :schema.dr_linkki_k ALTER COLUMN cost SET NOT NULL;
+ALTER TABLE :schema.dr_linkki_k ALTER COLUMN reverse_cost SET NOT NULL;
+
+ALTER TABLE :schema.dr_linkki_k DROP COLUMN geom_3d;

sql/transform_dr_linkki.sql
@@ -70,12 +70,4 @@ DROP TABLE :schema.dr_linkki_k;
 ALTER TABLE :schema.dr_linkki_out RENAME TO dr_linkki_k;
 
 -- Add data integrity constraints.
-ALTER TABLE :schema.dr_linkki_k ALTER COLUMN link_id SET NOT NULL;
-ALTER TABLE :schema.dr_linkki_k ALTER COLUMN segm_id SET NOT NULL;
-ALTER TABLE :schema.dr_linkki_k ALTER COLUMN kuntakoodi SET NOT NULL;
-ALTER TABLE :schema.dr_linkki_k ALTER COLUMN linkkityyp SET NOT NULL;
-ALTER TABLE :schema.dr_linkki_k ALTER COLUMN ajosuunta SET NOT NULL;
-
-ALTER TABLE :schema.dr_linkki_k ADD CONSTRAINT dr_linkki_k_pkey PRIMARY KEY (gid);
-ALTER TABLE :schema.dr_linkki_k ADD CONSTRAINT uk_dr_linkki_k_segm_id UNIQUE (segm_id);
-ALTER TABLE :schema.dr_linkki_k ADD CONSTRAINT fk_dr_linkki_k_link_id FOREIGN KEY (link_id) REFERENCES :schema.dr_link_id (link_id);
+\i :sql_dir/add_dr_linkki_k_constraints.sql

HSLdevcom/jore4-digiroad-import

export_pgdump_routing.sh
@@ -1,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Stop on first error. Have meaningful error messages. Print each command to stdout.
+set -euxo pipefail
+
+# Source common environment variables.
+source "$(cd "$(dirname "$0")"; pwd -P)/set_env_vars.sh"
+
+# Start Docker container. The container is expected to exist and contain all the data to be exported.
+docker start $DOCKER_CONTAINER
+
+# Wait for PostgreSQL to start.
+docker run -it --rm --link "${DOCKER_CONTAINER}":postgres $DOCKER_IMAGE sh -c "$PG_WAIT"
+
+PGDUMP_OUTPUT="digiroad_r_routing_$(date "+%Y-%m-%d").pgdump"
+OUTPUT_TABLES="dr_linkki dr_linkki_vertices_pgr dr_pysakki"
+OUTPUT_TABLE_OPTIONS="`echo ${OUTPUT_TABLES[@]} | sed \"s/dr_/-t ${DB_ROUTING_SCHEMA_NAME}.dr_/g\"`"
+
+# Export pg_dump file.
+docker run -it --rm --link "${DOCKER_CONTAINER}":postgres -v ${WORK_DIR}/pgdump/:/tmp/pgdump $DOCKER_IMAGE \
+  sh -c "$PG_DUMP -Fc --clean -f /tmp/pgdump/${PGDUMP_OUTPUT} $OUTPUT_TABLE_OPTIONS"
+
+# Stop Docker container.
+docker stop $DOCKER_CONTAINER

import_digiroad_shapefiles.sh
@@ -72,7 +72,7 @@ done
 
 # Process road geometries and filtering properties in database.
 docker run -it --rm --link "${DOCKER_CONTAINER}":postgres -v ${CWD}/sql:/tmp/sql \
-  ${DOCKER_IMAGE} sh -c "$PSQL -v ON_ERROR_STOP=1 -f /tmp/sql/transform_dr_linkki.sql -v schema=${DB_IMPORT_SCHEMA_NAME}"
+  ${DOCKER_IMAGE} sh -c "$PSQL -v ON_ERROR_STOP=1 -f /tmp/sql/transform_dr_linkki.sql -v schema=${DB_IMPORT_SCHEMA_NAME} -v sql_dir=/tmp/sql"
 
 # Load DR_PYSAKKI shapefile into database.
 docker run -it --rm --link "${DOCKER_CONTAINER}":postgres -v ${SHP_FILE_DIR}:/tmp/shp $DOCKER_IMAGE \
@@ -85,6 +85,10 @@ docker run -it --rm --link "${DOCKER_CONTAINER}":postgres -v ${CWD}/sql:/tmp/sql
 # Process turn restrictions and filter properties in database.
 docker run -it --rm --link "${DOCKER_CONTAINER}":postgres -v ${CWD}/sql:/tmp/sql \
   ${DOCKER_IMAGE} sh -c "$PSQL -v ON_ERROR_STOP=1 -f /tmp/sql/transform_dr_kaantymisrajoitus.sql -v schema=${DB_IMPORT_SCHEMA_NAME}"
+
+# Create routing schema containing pgRouting topology.
+docker run -it --rm --link "${DOCKER_CONTAINER}":postgres -v ${CWD}/sql:/tmp/sql \
+  ${DOCKER_IMAGE} sh -c "$PSQL -v ON_ERROR_STOP=1 -f /tmp/sql/create_routing_schema.sql -v source_schema=${DB_IMPORT_SCHEMA_NAME} -v schema=${DB_ROUTING_SCHEMA_NAME} -v sql_dir=/tmp/sql"
 
 # Create separate schema for exporting data in MBTiles format.
 docker run -it --rm --link "${DOCKER_CONTAINER}":postgres -v ${CWD}/sql:/tmp/sql \

sql/create_routing_schema.sql
@@ -1,0 +1,71 @@
+DROP SCHEMA IF EXISTS :schema CASCADE;
+
+CREATE SCHEMA :schema;
+
+CREATE TABLE :schema.dr_linkki AS
+SELECT
+    src.gid,
+    src.link_id,
+    src.kuntakoodi,
+    src.linkkityyp,
+    src.ajosuunta,
+    src.link_tila,
+    src.tienimi_su,
+    src.tienimi_ru,
+    src.tienimi_sa,
+    ST_Force3D(src.geom) AS geom_3d
+FROM :source_schema.dr_linkki src;
+
+ALTER TABLE :schema.dr_linkki ADD COLUMN source INTEGER;
+ALTER TABLE :schema.dr_linkki ADD COLUMN target INTEGER;
+
+ALTER TABLE :schema.dr_linkki ADD COLUMN cost FLOAT;
+ALTER TABLE :schema.dr_linkki ADD COLUMN reverse_cost FLOAT;
+
+-- Add 2D geometry to be used with pgRouting extension.
+SELECT AddGeometryColumn(:'schema', 'dr_linkki', 'geom', 3067, 'LINESTRING', 2);
+UPDATE :schema.dr_linkki SET geom = ST_Force2D(geom_3d);
+ALTER TABLE :schema.dr_linkki ALTER COLUMN geom SET NOT NULL;
+
+-- Add data integrity constraints to dr_linkki table.
+\i :sql_dir/add_dr_linkki_constraints.sql
+
+-- Copy dr_pysakki table from digiroad schema and add constraints.
+CREATE TABLE :schema.dr_pysakki (LIKE :source_schema.dr_pysakki);
+INSERT INTO :schema.dr_pysakki SELECT * FROM :source_schema.dr_pysakki;
+\i :sql_dir/add_dr_pysakki_constraints.sql
+
+-- Create pgRouting topology.
+
+CREATE INDEX dr_linkki_geom_idx ON :schema.dr_linkki USING GIST(geom);
+CREATE INDEX dr_linkki_source_idx ON :schema.dr_linkki (source);
+CREATE INDEX dr_linkki_target_idx ON :schema.dr_linkki (target);
+
+-- Need to use `gid` as ID column instead of `link_id` because pgRouting requires integer based ID column.
+SELECT pgr_createTopology(:'schema' || '.dr_linkki', 0.001, 'geom', 'gid');
+ALTER TABLE :schema.dr_linkki ALTER COLUMN source SET NOT NULL;
+ALTER TABLE :schema.dr_linkki ALTER COLUMN target SET NOT NULL;
+
+-- Set up cost and reverse_cost parameters for pgRouting functions.
+-- TODO: Do cost calculation based on speed limits.
+
+UPDATE :schema.dr_linkki
+SET cost = (
+    CASE
+        WHEN ajosuunta IN (2,4) THEN ST_Length(geom_3d)
+        ELSE -1
+    END
+);
+
+UPDATE :schema.dr_linkki
+SET reverse_cost = (
+    CASE
+        WHEN ajosuunta IN (2,3) THEN ST_Length(geom_3d)
+        ELSE -1
+    END
+);
+
+ALTER TABLE :schema.dr_linkki ALTER COLUMN cost SET NOT NULL;
+ALTER TABLE :schema.dr_linkki ALTER COLUMN reverse_cost SET NOT NULL;
+
+ALTER TABLE :schema.dr_linkki DROP COLUMN geom_3d;

sql/transform_dr_linkki.sql
@@ -73,10 +73,4 @@ DROP TABLE :schema.dr_linkki;
 ALTER TABLE :schema.dr_linkki_out RENAME TO dr_linkki;
 
 -- Add data integrity constraints.
-ALTER TABLE :schema.dr_linkki ALTER COLUMN link_id SET NOT NULL;
-ALTER TABLE :schema.dr_linkki ALTER COLUMN kuntakoodi SET NOT NULL;
-ALTER TABLE :schema.dr_linkki ALTER COLUMN linkkityyp SET NOT NULL;
-ALTER TABLE :schema.dr_linkki ALTER COLUMN ajosuunta SET NOT NULL;
-
-ALTER TABLE :schema.dr_linkki ADD CONSTRAINT dr_linkki_pkey PRIMARY KEY (gid);
-ALTER TABLE :schema.dr_linkki ADD CONSTRAINT uk_dr_linkki_link_id UNIQUE (link_id);
+\i :sql_dir/add_dr_linkki_constraints.sql