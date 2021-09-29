4c927cdc348a221357a316dfc73b20ec6e64d7bf
Tamas Pallos <tamas.pallos@vincit.fi>
2021-09-29 13:05:51 +0300

    Remove `--it` switches from docker runs
    These `--it` switches want to open an interactive shell which is not necessary and is also not available in github CI

--- export_pgdump_routing.sh
+++ export_pgdump_routing.sh
@@ -10,14 +10,14 @@ source "$(cd "$(dirname "$0")"; pwd -P)/set_env_vars.sh"
 docker start $DOCKER_CONTAINER
 
 # Wait for PostgreSQL to start.
-docker run -it --rm --link "${DOCKER_CONTAINER}":postgres $DOCKER_IMAGE sh -c "$PG_WAIT"
+docker exec "${DOCKER_CONTAINER}" sh -c "$PG_WAIT_LOCAL"
 
 PGDUMP_OUTPUT="digiroad_r_routing_$(date "+%Y-%m-%d").pgdump"
 OUTPUT_TABLES="dr_linkki dr_linkki_vertices_pgr dr_pysakki"
 OUTPUT_TABLE_OPTIONS="`echo ${OUTPUT_TABLES[@]} | sed \"s/dr_/-t ${DB_ROUTING_SCHEMA_NAME}.dr_/g\"`"
 
 # Export pg_dump file.
-docker run -it --rm --link "${DOCKER_CONTAINER}":postgres -v ${WORK_DIR}/pgdump/:/tmp/pgdump $DOCKER_IMAGE \
+docker run --rm --link "${DOCKER_CONTAINER}":postgres -v ${WORK_DIR}/pgdump/:/tmp/pgdump $DOCKER_IMAGE \
   sh -c "$PG_DUMP -Fc --clean -f /tmp/pgdump/${PGDUMP_OUTPUT} $OUTPUT_TABLE_OPTIONS"
 
 # Stop Docker container.
