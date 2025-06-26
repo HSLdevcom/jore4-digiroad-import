b49fdb7471680c19a8596ad7c05b1e2a64584c1b
Jarkko Kaura <jarkko.kaura@vincit.fi>
2025-06-26 16:54:25 +0300

    Add missing road links in Espoo and Kauniainen.
    Make it possible to convert bike paths into driveways.
    
    These fixes affect multiple routes and stop-to-stop segments.

--- sql/select_infra_links_as_csv.sql
+++ sql/select_infra_links_as_csv.sql
@@ -10,5 +10,26 @@ COPY (
         END AS direction,
         ST_Length(source.geom) as estimated_length_in_metres
     FROM :schema.dr_linkki_fixup source
-    WHERE source.ajosuunta IN (2, 3, 4) -- filter out possibly invalid links
+    WHERE
+        source.ajosuunta IN (2, 3, 4) -- filter out possibly invalid links
+        AND source.linkkityyp IN (
+            1, -- Moottoritien osa
+            2, -- Moniajorataisen tien osa, joka ei ole moottoritie
+            3, -- Yksiajorataisen tien osa
+            4, -- Moottoriliikennetien osa
+            5, -- Kiertoliittymän osa
+            6, -- Ramppi
+            7, -- Levähdysalue
+        --  8, -- Pyörätie tai yhdistetty pyörätie ja jalkakäytävä
+        --  9, -- Jalankulkualueen osa, esim. kävelykatu tai jalkakäytävä
+        -- 10, -- Huolto- tai pelastustien osa
+           11, -- Liitännäisliikennealueen osa
+           12, -- Ajopolku, maastoajoneuvolla ajettavissa olevat tiet
+        -- 13, -- Huoltoaukko moottoritiellä
+        -- 14, -- Erikoiskuljetusyhteys ilman puomia
+        -- 15, -- Erikoiskuljetusyhteys puomilla
+           21, -- Lossi
+           99  -- Ei tietoa (esiintyy vain rakenteilla olevilla tielinkeillä)
+        )
+
 ) TO STDOUT WITH (FORMAT CSV, HEADER)
