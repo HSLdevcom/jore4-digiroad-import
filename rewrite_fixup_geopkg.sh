3d0203aa52cc1aeff3e7799937e89ecd0a628d7d
Jarkko Kaura <jarkko.kaura@vincit.fi>
2025-08-21 17:20:21 +0300

    Add script to rewrite GeoPackage fixup file.
    This is a handy utility for adding HSL's supplementary infrastructure links to
    a GeoPackage file.

---
+++ rewrite_fixup_geopkg.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Stop on first error. Have meaningful error messages. Print each command to stdout.
+set -euxo pipefail
+
+# Source common environment variables.
+source "$(cd "$(dirname "$0")"; pwd -P)/set_env_vars.sh"
+
+# Start Docker container. The container is expected to exist and contain required database tables to be exported.
+docker start "$DOCKER_CONTAINER_NAME"
+
+# Wait for PostgreSQL server to be ready.
+docker exec "$DOCKER_CONTAINER_NAME" sh -c "$PG_WAIT"
+
+OGR_PG_SRC="PG:\"host=\$POSTGRES_PORT_5432_TCP_ADDR port=\$POSTGRES_PORT_5432_TCP_PORT user=digiroad dbname=$DB_NAME schemas=${DB_IMPORT_SCHEMA_NAME}\""
+
+docker run --rm --link "$DOCKER_CONTAINER_NAME":postgres -v "$CWD"/fixup/digiroad:/tmp/gpkg "$DOCKER_IMAGE" \
+  sh -c "ogr2ogr -f GPKG -overwrite /tmp/gpkg/fixup.gpkg $OGR_PG_SRC -nlt LINESTRINGZM -nln add_links -dim XYZM -sql \"SELECT * FROM fix_layer_link\""
+
+docker run --rm --link "$DOCKER_CONTAINER_NAME":postgres -v "$CWD"/fixup/digiroad:/tmp/gpkg "$DOCKER_IMAGE" \
+  sh -c "ogr2ogr -f GPKG -overwrite /tmp/gpkg/fixup.gpkg $OGR_PG_SRC -nln add_stop_points -sql \"SELECT * FROM fix_layer_stop_point\""
+
+docker run --rm --link "$DOCKER_CONTAINER_NAME":postgres -v "$CWD"/fixup/digiroad:/tmp/gpkg "$DOCKER_IMAGE" \
+  sh -c "ogr2ogr -f GPKG -overwrite /tmp/gpkg/fixup.gpkg $OGR_PG_SRC -nlt LINESTRINGZM -nln remove_links -dim XYZM -sql \"SELECT * FROM fix_layer_link_exclusion_geometry\""
+
+# Stop Docker container.
+docker stop "$DOCKER_CONTAINER_NAME"
