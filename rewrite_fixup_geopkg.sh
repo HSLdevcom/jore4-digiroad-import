463cc216a966ef745944e16da2d1c3dd25857fe8
Jarkko Kaura <jarkko.kaura@vincit.fi>
2025-08-25 13:26:34 +0300

    Modify the way the target database is specified.
    Make it a bit easier to write Digiroad data to a PostgreSQL database other than
    the one contained within the Docker container itself.

--- rewrite_fixup_geopkg.sh
+++ rewrite_fixup_geopkg.sh
@@ -12,16 +12,14 @@ docker start "$DOCKER_CONTAINER_NAME"
 # Wait for PostgreSQL server to be ready.
 docker exec "$DOCKER_CONTAINER_NAME" sh -c "$PG_WAIT"
 
-OGR_PG_SRC="PG:\"host=\$POSTGRES_PORT_5432_TCP_ADDR port=\$POSTGRES_PORT_5432_TCP_PORT user=digiroad dbname=$DB_NAME schemas=${DB_SCHEMA_NAME_DIGIROAD}\""
+docker run --rm --link "$DOCKER_CONTAINER_NAME":postgres -v "$CWD"/fixup/digiroad:/tmp/gpkg "$DOCKER_IMAGE" \
+  sh -c "ogr2ogr -f GPKG -overwrite /tmp/gpkg/fixup.gpkg $OGR2OGR_PG_REF -nlt LINESTRINGZM -nln add_links -dim XYZM -sql \"SELECT * FROM fix_layer_link\""
 
 docker run --rm --link "$DOCKER_CONTAINER_NAME":postgres -v "$CWD"/fixup/digiroad:/tmp/gpkg "$DOCKER_IMAGE" \
-  sh -c "ogr2ogr -f GPKG -overwrite /tmp/gpkg/fixup.gpkg $OGR_PG_SRC -nlt LINESTRINGZM -nln add_links -dim XYZM -sql \"SELECT * FROM fix_layer_link\""
+  sh -c "ogr2ogr -f GPKG -overwrite /tmp/gpkg/fixup.gpkg $OGR2OGR_PG_REF -nln add_stop_points -sql \"SELECT * FROM fix_layer_stop_point\""
 
 docker run --rm --link "$DOCKER_CONTAINER_NAME":postgres -v "$CWD"/fixup/digiroad:/tmp/gpkg "$DOCKER_IMAGE" \
-  sh -c "ogr2ogr -f GPKG -overwrite /tmp/gpkg/fixup.gpkg $OGR_PG_SRC -nln add_stop_points -sql \"SELECT * FROM fix_layer_stop_point\""
-
-docker run --rm --link "$DOCKER_CONTAINER_NAME":postgres -v "$CWD"/fixup/digiroad:/tmp/gpkg "$DOCKER_IMAGE" \
-  sh -c "ogr2ogr -f GPKG -overwrite /tmp/gpkg/fixup.gpkg $OGR_PG_SRC -nlt LINESTRINGZM -nln remove_links -dim XYZM -sql \"SELECT * FROM fix_layer_link_exclusion_geometry\""
+  sh -c "ogr2ogr -f GPKG -overwrite /tmp/gpkg/fixup.gpkg $OGR2OGR_PG_REF -nlt LINESTRINGZM -nln remove_links -dim XYZM -sql \"SELECT * FROM fix_layer_link_exclusion_geometry\""
 
 # Stop Docker container.
 docker stop "$DOCKER_CONTAINER_NAME"
