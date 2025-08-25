fe0489c0b10c70e3d2ec68fa8c94724d62cf968b
Jarkko Kaura <jarkko.kaura@vincit.fi>
2025-08-25 17:16:58 +0300

    Replace deprecated "docker run --link" commands with "docker exec".
    Remove `exec` prefix from environment variables set in `set_env_vars.sh`.
    This is done to have more clean code and separations concerned.
    
    Make shell scripts more concise by introducing the environment variables
    `DOCKER_EXEC_POSTGRES` and `DOCKER_EXEC_HOSTUSER`, the latter of which should be
    used when writing files intended to be shared with the Docker host user.

--- export_pgdump_digiroad.sh
+++ export_pgdump_digiroad.sh
@@ -9,8 +9,8 @@ source "$(dirname "$0")/set_env_vars.sh"
 # Start Docker container. The container is expected to exist and contain all the data to be exported.
 docker start "$DOCKER_CONTAINER_NAME"
 
-# Wait for PostgreSQL to start.
-docker run --rm --link "$DOCKER_CONTAINER_NAME":postgres "$DOCKER_IMAGE" sh -c "$PG_WAIT"
+# Wait for PostgreSQL server to be ready.
+$DOCKER_EXEC_POSTGRES "exec $PG_WAIT"
 
 PGDUMP_OUTPUT="digiroad_r_$(date "+%Y-%m-%d").pgdump"
 OUTPUT_TABLES="dr_linkki dr_pysakki dr_kaantymisrajoitus"
@@ -19,8 +19,7 @@ OUTPUT_TABLE_OPTIONS=$(echo "${OUTPUT_TABLES[@]}" | sed "s/dr_/-t ${DB_SCHEMA_NA
 mkdir -p "$WORK_DIR"/pgdump
 
 # Export pg_dump file.
-docker run --rm --link "$DOCKER_CONTAINER_NAME":postgres -v "$WORK_DIR"/pgdump/:/tmp/pgdump "$DOCKER_IMAGE" \
-  sh -c "$PG_DUMP -Fc --clean -f /tmp/pgdump/$PGDUMP_OUTPUT $OUTPUT_TABLE_OPTIONS"
+$DOCKER_EXEC_HOSTUSER "exec $PG_DUMP -Fc --clean -f /tmp/pgdump/$PGDUMP_OUTPUT $OUTPUT_TABLE_OPTIONS"
 
 # Stop Docker container.
 docker stop "$DOCKER_CONTAINER_NAME"
--- set_env_vars.sh
+++ set_env_vars.sh
@@ -12,6 +12,12 @@ export DOCKER_IMAGE="jore4/postgis-digiroad"
 export DOCKER_CONTAINER_NAME="jore4-postgis-digiroad"
 export DOCKER_CONTAINER_PORT="21000"
 
+export DOCKER_EXEC_POSTGRES="docker exec -u postgres $DOCKER_CONTAINER_NAME sh -c"
+# In Linux, UID/GID mapping provides correct permissions for files written inside
+# Docker container so that the Docker host user owns them.
+DOCKER_EXEC_HOSTUSER="docker exec -u $(id -u):$(id -g) $DOCKER_CONTAINER_NAME sh -c"
+export DOCKER_EXEC_HOSTUSER
+
 # Database details
 LOCAL_DB_NAME="digiroad"
 export DB_NAME="$LOCAL_DB_NAME"
@@ -20,13 +26,12 @@ export DB_SCHEMA_NAME_DIGIROAD="digiroad"
 export DB_SCHEMA_NAME_MBTILES="mbtiles"
 export DB_SCHEMA_NAME_ROUTING="routing"
 
-DB_HOST="\$POSTGRES_PORT_5432_TCP_ADDR"
-DB_PORT="\$POSTGRES_PORT_5432_TCP_PORT"
+DB_HOST="localhost"
+DB_PORT="5432"
 
 # Commands to run inside Docker container.
 export OGR2OGR_PG_REF="PG:\"host=$DB_HOST port=$DB_PORT dbname=$DB_NAME user=$DB_USERNAME schemas=$DB_SCHEMA_NAME_DIGIROAD\""
-export PGSQL2SHP="pgsql2shp -h \"$DB_HOST\" -p \"$DB_PORT\" -u $DB_USERNAME"
-export PG_DUMP="exec pg_dump -h \"$DB_HOST\" -p \"$DB_PORT\" -d $DB_NAME -U $DB_USERNAME --no-password"
-export PG_WAIT="exec /wait-pg.sh $DB_NAME \"$DB_HOST\" \"$DB_PORT\""
-export PG_WAIT_LOCAL="exec /wait-pg.sh $LOCAL_DB_NAME localhost 5432"
-export PSQL="exec psql -h \"$DB_HOST\" -p \"$DB_PORT\" -U $DB_USERNAME -d $DB_NAME --no-password"
+export PGSQL2SHP="pgsql2shp -h $DB_HOST -p $DB_PORT -u $DB_USERNAME"
+export PG_DUMP="pg_dump -h $DB_HOST -p $DB_PORT -d $DB_NAME -U $DB_USERNAME --no-password"
+export PG_WAIT="/wait-pg.sh $DB_NAME $DB_HOST $DB_PORT"
+export PSQL="psql -h $DB_HOST -p $DB_PORT -U $DB_USERNAME -d $DB_NAME --no-password"
