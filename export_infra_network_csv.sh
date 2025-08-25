6ce6679d157d276f5c386d4adfbcb17e5deaece5
Jarkko Kaura <jarkko.kaura@vincit.fi>
2025-08-25 17:16:58 +0300

    Replace deprecated "docker run --link" commands with "docker exec".
    Remove `exec` prefix from environment variables set in `set_env_vars.sh`.
    This is done to have more clean code and separations concerned.
    
    Make shell scripts more concise by introducing the environment variables
    `DOCKER_EXEC_POSTGRES` and `DOCKER_EXEC_HOSTUSER`, the latter of which should be
    used when writing files intended to be shared with the Docker host user.

--- export_infra_network_csv.sh
+++ export_infra_network_csv.sh
@@ -9,21 +9,18 @@ source "$(cd "$(dirname "$0")" || exit; pwd -P)/set_env_vars.sh"
 # Start Docker container. The container is expected to exist and contain all the data to be exported.
 docker start "$DOCKER_CONTAINER_NAME"
 
-# Wait for PostgreSQL to start.
-docker exec "$DOCKER_CONTAINER_NAME" sh -c "$PG_WAIT_LOCAL"
+# Wait for PostgreSQL server to be ready.
+$DOCKER_EXEC_POSTGRES "exec $PG_WAIT"
 
 # Export CSV file to output directory.
 OUTPUT_FILENAME="infra_network_digiroad.csv"
-OUTPUT_FOLDER="${WORK_DIR}/csv"
 
-mkdir -p "$OUTPUT_FOLDER"
+mkdir -p "${WORK_DIR}/csv"
 
 # Make sure infrastructure links are updated.
-docker exec "$DOCKER_CONTAINER_NAME" sh -c \
-  "$PSQL -nt -c \"REFRESH MATERIALIZED VIEW ${DB_SCHEMA_NAME_DIGIROAD}.dr_linkki_fixup;\""
+$DOCKER_EXEC_POSTGRES "exec $PSQL -nt -c \"REFRESH MATERIALIZED VIEW ${DB_SCHEMA_NAME_DIGIROAD}.dr_linkki_fixup;\""
 
-docker run --rm --link "$DOCKER_CONTAINER_NAME":postgres -v "$CWD"/sql:/tmp/sql -v "$OUTPUT_FOLDER":/tmp/csv "$DOCKER_IMAGE" \
-  sh -c "$PSQL -v ON_ERROR_STOP=1 -f /tmp/sql/select_infra_links_as_csv.sql -v schema=$DB_SCHEMA_NAME_DIGIROAD -o /tmp/csv/$OUTPUT_FILENAME"
+$DOCKER_EXEC_POSTGRES "exec $PSQL -v ON_ERROR_STOP=1 -f /tmp/sql/select_infra_links_as_csv.sql -v schema=$DB_SCHEMA_NAME_DIGIROAD -o /tmp/csv/$OUTPUT_FILENAME"
 
 # Stop Docker container.
 docker stop "$DOCKER_CONTAINER_NAME"
